4b6964a6db68c912f24c3824a625709371c37103
Yash Pal <112522911+yashpal2104@users.noreply.github.com>
2024-05-25 16:29:06 +0530

    Updated the getters and setters for the Train.java
    I have written the code for Train.java by using getters and setters in the editor

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -12,3 +12,63 @@ public class Train
     private Map<String, Time> stationTimes;
     private List<String> stations;
 }
+   public Train(String trainId, String trainNo, List<List<Integer>> seats, Time trainTime, Map<String, String> stationTime, List<String> stations) {
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.trainTime = trainTime;
+        this.stationTime = stationTime;
+        this.stations = stations;
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public Time getTrainTime() {
+        return trainTime;
+    }
+
+    public void setTrainTime(Time trainTime) {
+        this.trainTime = trainTime;
+    }
+
+    public Map<String, String> getStationTime() {
+        return stationTime;
+    }
+
+    public void setStationTime(Map<String, String> stationTime) {
+        this.stationTime = stationTime;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+    public String getTrainInfo(){
+        return String.format("Train ID: %s", trainId, trainNo);
+    }
+}
